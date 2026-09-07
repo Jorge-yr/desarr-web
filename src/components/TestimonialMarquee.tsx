0156a4a7eaa044470edd0ec6bd6ef641a017b258
Jorge-yr <136664491+Jorge-yr@users.noreply.github.com>
2026-09-07 00:55:20 -0300

    Add linkedin link en testimonio 5

--- src/components/TestimonialMarquee.tsx
+++ src/components/TestimonialMarquee.tsx
@@ -73,6 +73,8 @@ export const TESTIMONIALS: Testimonial[] = [
     company: "Top Neumáticos SRL",
     imageUrl: "/testimonials/Testimonio_5.jpg",
     imageKind: "portrait",
+    socialType: "LinkedIn",
+    socialUrl: "https://www.linkedin.com/in/alejandra-itati-sanchez/",
   },
 ];
 
